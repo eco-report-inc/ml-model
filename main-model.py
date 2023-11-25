a8838d93769267195d68580423f0abfce2898be6
nuijie <95111361+jihannurrahma@users.noreply.github.com>
2023-11-25 12:05:48 +0700

    Update main-model.py
    add callbacks

--- main-model.py
+++ main-model.py
@@ -3,13 +3,14 @@ from google.colab import drive
 drive.mount('/content/gdrive')
 
 # Define Root Directory to Datasets
-ROOT_DIR = '/content/gdrive/My Drive/...' # Fill in the blank with the path of your Datasets Directories
+ROOT_DIR = '/content/gdrive/My Drive/Eco Report'
 
 # Setting Environment
 !pip install ultralytics
 
-# Unzip Your Datasets (Do it one time only, if you got disconnected from Google Collab, run all the cell except this one)
-1unzip "/content/gdrive/MyDrive/...(your dataset path)" -d "/content/gdrive/MyDrive/...(your desired path for unzip datasets)"
+# Unzip Datasets (One time only)
+!mkdir '/content/gdrive/My Drive/Eco Report/Datasets'
+!unzip "/content/gdrive/MyDrive/Eco Report/aggra.v2i.yolov8.zip)" -d "/content/gdrive/MyDrive/Eco Report/Datasets)"
 
 # Import Dataset Model
 import os
@@ -18,11 +19,18 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO("yolov8n.yaml")
 
+# Callbacks
+!mkdir '/content/gdrive/My Drive/Eco Report/logs'
+data_config= os.path.join(ROOT_DIR, "trash-config.yaml")
+log_dir= '/content/gdrive/My Drive/Eco Report/logs'
+tensorboard_callback = model.train(data= data_config,
+                                   epochs= 100,
+                                   project= log_dir,
+                                   )
 # Use the model
-model.train(data= os.path.join(ROOT_DIR, "trash-config.yaml"), epochs=3) # train the model and custom the epoch to reach your desired accuracy
+model.train()
 
 # Result Check
 from IPython.display import display, Image
-Image(filename=f'(fill it with your result.png path)', width=600)
-
-Image(filename=f'(fill it with your confusion_matrix.png path)', width=600)
+Image(filename=f'(/content/gdrive/My Drive/Eco Report/logs/train/result.png)', width=600)
+Image(filename=f'(/content/gdrive/My Drive/Eco Report/logs/train/confusion_matrix.png)', width=600)
