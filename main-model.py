9b55d7aa55c1b111af3e2a9a50e32ef69a54e851
nuijie <95111361+jihannurrahma@users.noreply.github.com>
2023-12-13 13:40:30 +0700

    Update main-model.py
    add model train step

--- main-model.py
+++ main-model.py
@@ -3,13 +3,13 @@ from google.colab import drive
 drive.mount('/content/gdrive')
 
 # Define Root Directory to Datasets
-ROOT_DIR = '/content/gdrive/My Drive/Eco Report'
+ROOT_DIR = '/content/gdrive/My Drive/EcoReport'
 
 # Setting Environment
 !pip install ultralytics
 
 # Unzip Datasets (One time only)
-!unzip "/content/gdrive/MyDrive/Eco Report/aggra.v2i.yolov8.zip" -d "/content/gdrive/MyDrive/Eco Report/Datasets"
+!unzip "/content/gdrive/MyDrive/EcoReport/train5-20231213T045433Z-001.zip" -d "/content/gdrive/MyDrive/EcoReport/logs"
 
 # Import Library
 import os
@@ -17,17 +17,23 @@ import shutil
 from ultralytics import YOLO
 from IPython.display import display, Image
 
-# Load a model
+# Base Model & Training Logs
 model = YOLO("yolov8n.yaml")
-
-# Training Logs
 data_config= os.path.join(ROOT_DIR, "trash-config.yaml")
 log_dir= '/content/gdrive/My Drive/Eco Report/logs'
 
-# Train a model
+# Train and Validate model
 model.train(data= data_config,
-            epochs= 150,
-            project= log_dir,
+            pretrained= False,
+            epochs= 300,
+            batch= 16,
+            imgsz= 640,
+            optimizer= 'Adam',
+            lr0=0.001,
+            weight_decay=0.0005,
+            patience= 50,
+            val= True,
+            project= logs_dir,
             )
 
 # Train Result Check
@@ -38,21 +44,10 @@ print(contents)
 Image(filename=f'/content/gdrive/My Drive/Eco Report/logs/train/results.png', width=600)
 Image(filename=f'/content/gdrive/My Drive/Eco Report/logs/train/confusion_matrix.png', width=600)
 
-# Validate Model
-output_directory = '/content/gdrive/MyDrive/EcoReport'
-!yolo task=detect mode=val model='/content/gdrive/MyDrive/EcoReport/logs/train8/weights/best.pt' data= '/content/gdrive/MyDrive/EcoReport/data.yaml'
-
-shutil.move('/content/runs/detect/val', output_directory)
-
-# Check Validation Result
-Image(filename=f'/content/gdrive/MyDrive/EcoReport/val/confusion_matrix.png', width=600)
-
-# Predict Model
-model= YOLO('/content/gdrive/MyDrive/Eco Report/logs/train4/weights/best.pt')
+# Predict Model in Local
+model= YOLO('/content/gdrive/MyDrive/Eco Report/logs/train/weights/best.pt')
 source= '/content/gdrive/MyDrive/Eco Report/Datasets/test/images/-libre-place-des-dejections-en-plusieurs-langues-photo-marc-wirtz-1430300655_jpg.rf.0d5eae0d8975922bffeafd619e49f2f5.jpg'
-result= model(source)
-
-model.predict(source, save=True, imgsz=640, conf=0.25)
+result= model.predict(source, save=True, imgsz=640, conf=0.25)
 
 # Check Prediction Result
 Image(filename=f'/content/runs/detect/predict2/-libre-place-des-dejections-en-plusieurs-langues-photo-marc-wirtz-1430300655_jpg.rf.0d5eae0d8975922bffeafd619e49f2f5.jpg', width=600)
@@ -62,5 +57,5 @@ output_directory = '/content/gdrive/MyDrive/EcoReport/Result'
 shutil.move('/content/runs/detect/predict', output_directory)
 
 #Export Model format ONNX
-model= YOLO('/content/gdrive/MyDrive/EcoReport/logs/train2/weights/best.pt')
-model.export(format='onnx')
+model= YOLO('/content/gdrive/MyDrive/EcoReport/logs/train/weights/best.pt')
+model.export(format='tflite')
